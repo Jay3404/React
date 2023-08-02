9eeddb3d4cdc0302bffd0f571cbb2872c82a381c
Jay3404 <111181171+Jay3404@users.noreply.github.com>
2023-08-02 12:17:44 +0900

    db에서 boardList 호출

--- board-app/src/components/BoardList.js
+++ board-app/src/components/BoardList.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import BoardListItem from './BoardListItem';
-import {Link} from 'react-router-dom';
 
 const BoardList = () => {
     const [boardList, setBoardList] = useState([
@@ -45,43 +46,59 @@ const BoardList = () => {
             boardCnt: 0
         },
     ]);
-  return (
-    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <h3>게시글 목록</h3>
-        <form id="searchForm" >
-            <table style={{width: '700px', borderCollapse: 'collapse', border: '1px solid black'}}>
-            <tr>
-                <td style={{textAlign: 'right'}}>
-                <select name="searchCondition">
-                    <option value="all">전체</option>
-                    <option value="title">제목</option>
-                    <option value="content">내용</option>
-                    <option value="writer">작성자</option>
-                </select>
-                <input type="text" name="searchKeyword"></input>
-                <button type="button" id="btnSearch">검색</button>
-                </td>
-            </tr>
-            </table>
-        </form>
 
-        <table id="boardTable" style={{width: '700px', borderCollapse: 'collapse', border: '1px solid black'}}>
-            <tr>
-                <th style={{background: 'skyblue', width: '100px'}}>번호</th>
-                <th style={{background: 'skyblue', width: '200px'}}>제목</th>
-                <th style={{background: 'skyblue', width: '150px'}}>작성자</th>
-                <th style={{background: 'skyblue', width: '150px'}}>등록일</th>
-                <th style={{background: 'skyblue', width: '100px'}}>조회수</th>
-            </tr>
-            {boardList && boardList.map(board => <BoardListItem key={board.boardNo} board={board}></BoardListItem>)}
-            
-        </table>
-        <br/>
-        
-        <br/>
-        <Link to="/insert-board">새 글 등록</Link>
-    </div>
-  );
+    useEffect(() => {
+        const loadBoardList = async () => {
+            try {
+                const response = await axios.get('http://localhost:9090/board/board-list');
+                console.log(response);
+                setBoardList(response.data.pageItems.content);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        loadBoardList();
+    }, [])
+
+
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <h3>게시글 목록</h3>
+            <form id="searchForm" >
+                <table style={{ width: '700px', borderCollapse: 'collapse', border: '1px solid black' }}>
+                    <tr>
+                        <td style={{ textAlign: 'right' }}>
+                            <select name="searchCondition">
+                                <option value="all">전체</option>
+                                <option value="title">제목</option>
+                                <option value="content">내용</option>
+                                <option value="writer">작성자</option>
+                            </select>
+                            <input type="text" name="searchKeyword"></input>
+                            <button type="button" id="btnSearch">검색</button>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+
+            <table id="boardTable" style={{ width: '700px', borderCollapse: 'collapse', border: '1px solid black' }}>
+                <tr>
+                    <th style={{ background: 'skyblue', width: '100px' }}>번호</th>
+                    <th style={{ background: 'skyblue', width: '200px' }}>제목</th>
+                    <th style={{ background: 'skyblue', width: '150px' }}>작성자</th>
+                    <th style={{ background: 'skyblue', width: '150px' }}>등록일</th>
+                    <th style={{ background: 'skyblue', width: '100px' }}>조회수</th>
+                </tr>
+                {boardList && boardList.map(board => <BoardListItem key={board.boardNo} board={board}></BoardListItem>)}
+
+            </table>
+            <br />
+
+            <br />
+            <Link to="/insert-board">새 글 등록</Link>
+        </div>
+    );
 };
 
 export default BoardList;
